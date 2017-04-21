e134ce4b4fb3cdbfe7bf8fefe71c45656b7d2964
Brad <bderstine@americanbible.org>
2017-04-20 22:14:42 -0400

    add scripts, update welcome_text

---
+++ Assets/Scripts/ChooseTeamScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseTeamScript : MonoBehaviour {
+	public Text welcome_text;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(MainMenu.myName);
+		welcome_text.text = "Welcome " + MainMenu.myName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public InputField nameField;
+	public static string myName;
+
+	public void SubmitName(int sceneIndex) {
+		myName = nameField.text;
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
