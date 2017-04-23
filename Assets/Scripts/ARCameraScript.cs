611ce364c9b47a5a48d8cc858ac49a79558cebcf
Brad <bderstine@americanbible.org>
2017-04-23 16:31:54 -0400

    add effects, looping

--- Assets/Scripts/ARCameraScript.cs
+++ Assets/Scripts/ARCameraScript.cs
@@ -13,9 +13,23 @@ public class ARCameraScript : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
+	public GameObject monitor;
 	// Update is called once per frame
 	void Update () {
-		
+		monitor = GameObject.Find("energyBlast");
+		if (Input.GetMouseButtonDown(0)) {
+			Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rhInfo;
+			bool didHit = Physics.Raycast(toMouse, out rhInfo, 500.0f);
+			if (didHit) {
+				Debug.Log(rhInfo.collider.name + " .. " + rhInfo.point);
+				if (monitor) {
+					SceneManager.LoadScene(2);
+				}
+			} else {
+				Debug.Log("clicked on empty space");
+			}
+		}
 	}
 }
