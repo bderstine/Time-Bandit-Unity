2e19a8e0a01cad77561b53eb66c877b04fb62c07
Brad <bderstine@americanbible.org>
2017-04-23 15:55:38 -0400

    add arcamera scene and script

---
+++ Assets/Scripts/CasesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CasesScript : MonoBehaviour {
+
+	public void OpenScene (int sceneIndex) {
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
