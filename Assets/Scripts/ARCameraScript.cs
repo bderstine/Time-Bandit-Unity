af204c81fb02523eaa2d0d6373a1c2e8f3af6d35
Brad <bderstine@americanbible.org>
2017-04-25 10:06:16 -0400

    add font and background image

--- Assets/Scripts/ARCameraScript.cs
+++ Assets/Scripts/ARCameraScript.cs
@@ -14,22 +14,27 @@ public class ARCameraScript : MonoBehaviour {
 		
 	}
 
-	public GameObject monitor;
-	// Update is called once per frame
-	void Update () {
-		monitor = GameObject.Find("energyBlast");
-		if (Input.GetMouseButtonDown(0)) {
-			Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit rhInfo;
-			bool didHit = Physics.Raycast(toMouse, out rhInfo, 500.0f);
-			if (didHit) {
-				Debug.Log(rhInfo.collider.name + " .. " + rhInfo.point);
-				if (monitor) {
-					SceneManager.LoadScene(2);
-				}
-			} else {
-				Debug.Log("clicked on empty space");
+	Vector3 touchPosWorld;
+	TouchPhase touchPhase = TouchPhase.Began;
+	void Update() {
+		//We check if we have more than one touch happening.
+		//We also check if the first touches phase is Ended (that the finger was lifted)
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase) {
+			//We transform the touch position into word space from screen space and store it.
+			touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+			Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+
+			//We now raycast with this information. If we have hit something we can process it.
+			RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+			if (hitInformation.collider != null) {
+				//We should have hit something with a 2D Physics collider!
+				GameObject touchedObject = hitInformation.transform.gameObject;
+				//touchedObject should be the object someone touched.
+				Debug.Log("Touched " + touchedObject.transform.name);
 			}
 		}
 	}
+
 }
